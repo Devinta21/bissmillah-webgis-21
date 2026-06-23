ab24359dfc3671bc0ca01e9c14c01f91803702a2
Devinta21 <devintat93@gmail.com>
2026-06-23 19:30:54 +0700

    Add files via upload

---
+++ layers/Servicearealines_4.js
@@ -0,0 +1,1 @@
+var json_Servicearealines_4 = {"type":"FeatureCollection","name":"Servicearealines_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ layers/layers.js
@@ -0,0 +1,127 @@
+var wms_layers = [];
+
+
+        var lyr_Positron_0 = new ol.layer.Tile({
+            'title': 'Positron',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '<a href="https://cartodb.com/basemaps/">Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>',
+                url: 'https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+            })
+        });
+var format_ADMINISTRASIDESA_AR_25K_1 = new ol.format.GeoJSON();
+var features_ADMINISTRASIDESA_AR_25K_1 = format_ADMINISTRASIDESA_AR_25K_1.readFeatures(json_ADMINISTRASIDESA_AR_25K_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ADMINISTRASIDESA_AR_25K_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ADMINISTRASIDESA_AR_25K_1.addFeatures(features_ADMINISTRASIDESA_AR_25K_1);
+var lyr_ADMINISTRASIDESA_AR_25K_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ADMINISTRASIDESA_AR_25K_1, 
+                style: style_ADMINISTRASIDESA_AR_25K_1,
+                popuplayertitle: 'ADMINISTRASIDESA_AR_25K',
+                interactive: true,
+                title: '<img src="styles/legend/ADMINISTRASIDESA_AR_25K_1.png" /> ADMINISTRASIDESA_AR_25K'
+            });
+var format_JALAN_LN_25K_2 = new ol.format.GeoJSON();
+var features_JALAN_LN_25K_2 = format_JALAN_LN_25K_2.readFeatures(json_JALAN_LN_25K_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JALAN_LN_25K_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JALAN_LN_25K_2.addFeatures(features_JALAN_LN_25K_2);
+var lyr_JALAN_LN_25K_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_JALAN_LN_25K_2, 
+                style: style_JALAN_LN_25K_2,
+                popuplayertitle: 'JALAN_LN_25K',
+                interactive: true,
+                title: '<img src="styles/legend/JALAN_LN_25K_2.png" /> JALAN_LN_25K'
+            });
+var format_Buffered_3 = new ol.format.GeoJSON();
+var features_Buffered_3 = format_Buffered_3.readFeatures(json_Buffered_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Buffered_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Buffered_3.addFeatures(features_Buffered_3);
+var lyr_Buffered_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Buffered_3, 
+                style: style_Buffered_3,
+                popuplayertitle: 'Buffered',
+                interactive: true,
+                title: '<img src="styles/legend/Buffered_3.png" /> Buffered'
+            });
+var format_Servicearealines_4 = new ol.format.GeoJSON();
+var features_Servicearealines_4 = format_Servicearealines_4.readFeatures(json_Servicearealines_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Servicearealines_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Servicearealines_4.addFeatures(features_Servicearealines_4);
+var lyr_Servicearealines_4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Servicearealines_4, 
+                style: style_Servicearealines_4,
+                popuplayertitle: 'Service area (lines)',
+                interactive: true,
+                title: '<img src="styles/legend/Servicearealines_4.png" /> Service area (lines)'
+            });
+var format_PENDIDIKAN_PT_25K_5 = new ol.format.GeoJSON();
+var features_PENDIDIKAN_PT_25K_5 = format_PENDIDIKAN_PT_25K_5.readFeatures(json_PENDIDIKAN_PT_25K_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_PENDIDIKAN_PT_25K_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_PENDIDIKAN_PT_25K_5.addFeatures(features_PENDIDIKAN_PT_25K_5);
+var lyr_PENDIDIKAN_PT_25K_5 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_PENDIDIKAN_PT_25K_5, 
+                style: style_PENDIDIKAN_PT_25K_5,
+                popuplayertitle: 'PENDIDIKAN_PT_25K',
+                interactive: true,
+                title: '<img src="styles/legend/PENDIDIKAN_PT_25K_5.png" /> PENDIDIKAN_PT_25K'
+            });
+var format_Reprojectedpendidikan_6 = new ol.format.GeoJSON();
+var features_Reprojectedpendidikan_6 = format_Reprojectedpendidikan_6.readFeatures(json_Reprojectedpendidikan_6, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Reprojectedpendidikan_6 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Reprojectedpendidikan_6.addFeatures(features_Reprojectedpendidikan_6);
+var lyr_Reprojectedpendidikan_6 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Reprojectedpendidikan_6, 
+                style: style_Reprojectedpendidikan_6,
+                popuplayertitle: 'Reprojected pendidikan',
+                interactive: true,
+                title: '<img src="styles/legend/Reprojectedpendidikan_6.png" /> Reprojected pendidikan'
+            });
+
+lyr_Positron_0.setVisible(true);lyr_ADMINISTRASIDESA_AR_25K_1.setVisible(true);lyr_JALAN_LN_25K_2.setVisible(true);lyr_Buffered_3.setVisible(true);lyr_Servicearealines_4.setVisible(true);lyr_PENDIDIKAN_PT_25K_5.setVisible(true);lyr_Reprojectedpendidikan_6.setVisible(true);
+var layersList = [lyr_Positron_0,lyr_ADMINISTRASIDESA_AR_25K_1,lyr_JALAN_LN_25K_2,lyr_Buffered_3,lyr_Servicearealines_4,lyr_PENDIDIKAN_PT_25K_5,lyr_Reprojectedpendidikan_6];
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_JALAN_LN_25K_2.set('fieldAliases', {'NAMRJL': 'NAMRJL', 'KONRJL': 'KONRJL', 'MATRJL': 'MATRJL', 'FGSRJL': 'FGSRJL', 'UTKRJL': 'UTKRJL', 'TOLRJL': 'TOLRJL', 'WLYRJL': 'WLYRJL', 'AUTRJL': 'AUTRJL', 'KLSRJL': 'KLSRJL', 'SPCRJL': 'SPCRJL', 'JPARJL': 'JPARJL', 'ARHRJL': 'ARHRJL', 'STARJL': 'STARJL', 'KLLRJL': 'KLLRJL', 'MEDRJL': 'MEDRJL', 'LOCRJL': 'LOCRJL', 'JARRJL': 'JARRJL', 'FCODE': 'FCODE', 'REMARK': 'REMARK', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'SHAPE_Leng': 'SHAPE_Leng', });
+lyr_Buffered_3.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'LUAS': 'LUAS', 'KATPDK': 'KATPDK', 'JLPDDK': 'JLPDDK', 'FGGPDK': 'FGGPDK', 'REMARK': 'REMARK', 'FCODE': 'FCODE', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'JJGPDF': 'JJGPDF', 'JNSPDL': 'JNSPDL', 'type': 'type', 'start': 'start', });
+lyr_Servicearealines_4.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'LUAS': 'LUAS', 'KATPDK': 'KATPDK', 'JLPDDK': 'JLPDDK', 'FGGPDK': 'FGGPDK', 'REMARK': 'REMARK', 'FCODE': 'FCODE', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'JJGPDF': 'JJGPDF', 'JNSPDL': 'JNSPDL', 'type': 'type', 'start': 'start', });
+lyr_PENDIDIKAN_PT_25K_5.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'LUAS': 'LUAS', 'KATPDK': 'KATPDK', 'JLPDDK': 'JLPDDK', 'FGGPDK': 'FGGPDK', 'REMARK': 'REMARK', 'FCODE': 'FCODE', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'JJGPDF': 'JJGPDF', 'JNSPDL': 'JNSPDL', });
+lyr_Reprojectedpendidikan_6.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'LUAS': 'LUAS', 'KATPDK': 'KATPDK', 'JLPDDK': 'JLPDDK', 'FGGPDK': 'FGGPDK', 'REMARK': 'REMARK', 'FCODE': 'FCODE', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'JJGPDF': 'JJGPDF', 'JNSPDL': 'JNSPDL', });
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldImages', {'KDPPUM': '', 'NAMOBJ': '', 'REMARK': '', 'KDPBPS': '', 'FCODE': '', 'LUASWH': '', 'UUPP': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'KDEBPS': '', 'KDEPUM': '', 'KDCBPS': '', 'KDCPUM': '', 'KDBBPS': '', 'KDBPUM': '', 'WADMKD': '', 'WIADKD': '', 'WADMKC': '', 'WIADKC': '', 'WADMKK': '', 'WIADKK': '', 'WADMPR': '', 'WIADPR': '', 'TIPADM': '', 'SHAPE_Leng': '', 'SHAPE_Area': '', });
+lyr_JALAN_LN_25K_2.set('fieldImages', {'NAMRJL': '', 'KONRJL': '', 'MATRJL': '', 'FGSRJL': '', 'UTKRJL': '', 'TOLRJL': '', 'WLYRJL': '', 'AUTRJL': '', 'KLSRJL': '', 'SPCRJL': '', 'JPARJL': '', 'ARHRJL': '', 'STARJL': '', 'KLLRJL': '', 'MEDRJL': '', 'LOCRJL': '', 'JARRJL': '', 'FCODE': '', 'REMARK': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'SHAPE_Leng': '', });
+lyr_Buffered_3.set('fieldImages', {'NAMOBJ': 'TextEdit', 'LUAS': 'TextEdit', 'KATPDK': 'TextEdit', 'JLPDDK': 'TextEdit', 'FGGPDK': 'TextEdit', 'REMARK': 'TextEdit', 'FCODE': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'JJGPDF': 'TextEdit', 'JNSPDL': 'TextEdit', 'type': 'TextEdit', 'start': 'TextEdit', });
+lyr_Servicearealines_4.set('fieldImages', {'NAMOBJ': '', 'LUAS': '', 'KATPDK': '', 'JLPDDK': '', 'FGGPDK': '', 'REMARK': '', 'FCODE': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'JJGPDF': '', 'JNSPDL': '', 'type': '', 'start': '', });
+lyr_PENDIDIKAN_PT_25K_5.set('fieldImages', {'NAMOBJ': 'TextEdit', 'LUAS': 'TextEdit', 'KATPDK': 'TextEdit', 'JLPDDK': 'TextEdit', 'FGGPDK': 'TextEdit', 'REMARK': 'TextEdit', 'FCODE': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'JJGPDF': 'TextEdit', 'JNSPDL': 'TextEdit', });
+lyr_Reprojectedpendidikan_6.set('fieldImages', {'NAMOBJ': '', 'LUAS': '', 'KATPDK': '', 'JLPDDK': '', 'FGGPDK': '', 'REMARK': '', 'FCODE': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'JJGPDF': '', 'JNSPDL': '', });
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'no label', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_JALAN_LN_25K_2.set('fieldLabels', {'NAMRJL': 'no label', 'KONRJL': 'no label', 'MATRJL': 'no label', 'FGSRJL': 'no label', 'UTKRJL': 'no label', 'TOLRJL': 'no label', 'WLYRJL': 'no label', 'AUTRJL': 'no label', 'KLSRJL': 'no label', 'SPCRJL': 'no label', 'JPARJL': 'no label', 'ARHRJL': 'no label', 'STARJL': 'no label', 'KLLRJL': 'no label', 'MEDRJL': 'no label', 'LOCRJL': 'no label', 'JARRJL': 'no label', 'FCODE': 'no label', 'REMARK': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'SHAPE_Leng': 'no label', });
+lyr_Buffered_3.set('fieldLabels', {'NAMOBJ': 'no label', 'LUAS': 'no label', 'KATPDK': 'no label', 'JLPDDK': 'no label', 'FGGPDK': 'no label', 'REMARK': 'no label', 'FCODE': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'JJGPDF': 'no label', 'JNSPDL': 'no label', 'type': 'no label', 'start': 'no label', });
+lyr_Servicearealines_4.set('fieldLabels', {'NAMOBJ': 'no label', 'LUAS': 'no label', 'KATPDK': 'no label', 'JLPDDK': 'no label', 'FGGPDK': 'no label', 'REMARK': 'no label', 'FCODE': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'JJGPDF': 'no label', 'JNSPDL': 'no label', 'type': 'no label', 'start': 'no label', });
+lyr_PENDIDIKAN_PT_25K_5.set('fieldLabels', {'NAMOBJ': 'no label', 'LUAS': 'no label', 'KATPDK': 'no label', 'JLPDDK': 'no label', 'FGGPDK': 'no label', 'REMARK': 'no label', 'FCODE': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'JJGPDF': 'no label', 'JNSPDL': 'no label', });
+lyr_Reprojectedpendidikan_6.set('fieldLabels', {'NAMOBJ': 'header label - visible with data', 'LUAS': 'no label', 'KATPDK': 'no label', 'JLPDDK': 'no label', 'FGGPDK': 'no label', 'REMARK': 'no label', 'FCODE': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'JJGPDF': 'no label', 'JNSPDL': 'no label', });
+lyr_Reprojectedpendidikan_6.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
